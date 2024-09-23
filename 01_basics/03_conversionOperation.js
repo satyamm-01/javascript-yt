41e818e2570db293896b63459a0edbee455264d7
satyamm-01 <sr38791977@gmail.com>
2024-09-23 08:10:07 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let age = "33abc"
+
+console.log(typeof age);
+console.log(typeof (age));
+
+let valueInt = Number(age)
+console.log(typeof valueInt);
+console.log(valueInt);
+
+
+// "33" => 33
+// "33abc => NaN
+// true => 1; false => 0
+
+let login = "satyam"
+
+let booleanLogin = Boolean(login)
+console.log(booleanLogin)
+
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
